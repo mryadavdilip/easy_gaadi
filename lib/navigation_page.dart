d1b192ba4808f7c21764ce8592b280cbb7b59104
Gulshan Yadav <ry4761@gmail.com>
2023-05-24 17:25:46 +0530

    complete

--- lib/navigation_page.dart
+++ lib/navigation_page.dart
@@ -4,7 +4,6 @@ import 'package:easy_gaadi/home_page.dart';
 import 'package:easy_gaadi/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class NavigationPage extends StatefulWidget {
   const NavigationPage({super.key});
